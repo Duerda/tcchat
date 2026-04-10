c978619079f8bb1e9ce7840bebdf628f4a2529c3
Duerda <Eduarda.teixeira13@etec.sp.gov.br>
2026-04-10 16:52:18 -0300

    corrigi a repita a senha
    Refactor form validation and improve error handling for required fields and password matching.

--- Inicial-tela/Cadastro/Cad-back.js
+++ Inicial-tela/Cadastro/Cad-back.js
@@ -1,35 +1,38 @@
 console.log("Arquivo JS carregou com sucesso!"); // TESTE
 
-function Entraralpr(){   
+function Entraralpr() {
     window.location.href = "../../Professor/index.html";
 }
-function Logar(){
+function Logar() {
     window.location.href = "../Login/index.html";
 }
-function Formulario (event){
+function Formulario(event) {
     // Pega as informações dos inputs que tem required (todos)
     const campos = document.querySelectorAll("input[required]");
     let camposVazios = false;
     // Vai verificar se tem algum campo vazio
     campos.forEach((campo) => {
-        if(campo.value === ""){
+        if (campo.value === "") {
             camposVazios = true;
             campo.style.border = "1px solid red"; //Vai destacar em vermelho campo que está vazio
         }
-        else{
+        else {
             campo.style.border = "none"; // É para caso ele não esteja não vai colocar nada
         }
     });
-     if (camposVazios) {
-        alert("Por favor, preencha todos os campos obrigatórios!");
-        event.preventDefault(); // impede o envio do formulário
-        return false;
-    }
+
+    const senha = document.getElementById("Senha").value;
+    const repitaSenha = document.getElementById("RepitaSenha").value;
+
     if (senha !== repitaSenha) {
         alert("As senhas não coincidem. Por favor, tente novamente.");
-        document.querySelector("#Repita-senha input").style.border = "1px solid red";
-        event.preventDefault(); // impede o envio do formulário
+        const campoRepetir = document.getElementById("RepitaSenha"); campoRepetir.style.border = "1px solid red";
+        campoRepetir.focus(); // Coloca o cursor no campo errado
+        event.preventDefault(); // Impede o envio do formulário
         return false;
     }
+
+    }
+
 }
     
