dcd11795cbce768275a0774155bb62e569f6eb8c
Pizza104 <167818115+Pizza104@users.noreply.github.com>
2026-04-17 15:18:48 -0300

    O joão tem que arrumar tava dando erro.
    Removed the Voltar function and DOMContentLoaded event listeners that set text content from local storage.

--- Aluno/Turma.js
+++ Aluno/Turma.js
@@ -1,13 +1,1 @@
-function Voltar (){
-    window.location.href = "/Inicial-tela/Cadastro/Cad.html"
-}
-document.addEventListener('DOMContentLoaded', function() {
-    const spanIniciais = document.getElementById("foto").querySelector("span"); // Seleciona o span dentro de #foto
-    const iniciaisSalvas = localStorage.getItem("iniciaisUsuario");
-    spanIniciais.textContent = iniciaisSalvas || ""; // Define o texto ou vazio
-});
-document.addEventListener('DOMContentLoaded', function() {
-    const spanIniciais = document.getElementById("NomeUC").querySelector("h4"); // Seleciona o span dentro de #foto
-    const nomeUsuario = localStorage.getItem("nomeUsuario");
-    spanIniciais.textContent = nomeUsuario || ""; // Define o texto ou vazio
-});
+
