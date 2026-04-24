6711c242764c3d33a58d02336b62cd4d6df40daa
Duerda <Eduarda.teixeira13@etec.sp.gov.br>
2026-04-24 16:38:01 -0300

    Configure Supabase client in config.js
    Import Supabase client and export instance for API calls.

--- api/config.js
+++ api/config.js
@@ -1,8 +1,10 @@
-// Configuração do Supabase
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
 const SUPABASE_URL = "https://ktnlazcmojcrzxspggyf.supabase.co"; 
 const SUPABASE_KEY = "sb_publishable_NT8jHzUdqXM8lgL2Pfn2UQ_1W0IzCWH"; 
 
-// Função para chamar API
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY );
+
 async function chamadaSupabase(tabela, metodo = 'GET', dados = null) {
     const url = `${SUPABASE_URL}/rest/v1/${tabela}`;
     
