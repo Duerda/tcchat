1adac6f2e300eaf070f15d98b5fd281f1a45bab8
Duerda <Eduarda.teixeira13@etec.sp.gov.br>
2026-04-29 18:18:56 -0300

    Delete api directory

--- api/config.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const SUPABASE_URL = "https://ktnlazcmojcrzxspggyf.supabase.co"; 
-const SUPABASE_KEY = "sb_publishable_NT8jHzUdqXM8lgL2Pfn2UQ_1W0IzCWH"; 
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY );
