ef0224aa9347fca4f5399cdf91bcac3aaa068a37
kaua <kauasilvateen@gmail.com>
2026-05-06 10:08:23 -0300

    Testando para ver o meu commit

--- Inicial-tela/Login/Log-prof.js
+++ Inicial-tela/Login/Log-prof.js
@@ -1,11 +1,11 @@
 
-function Entrar(){
-    window.location.href = "./Professor/index.html";
+window.Entrar = function(){
+    window.location.href = "/Professor/Index.html";
 }
-function Aluno(){
+window.Aluno = function(){
     window.location.href = "Log-aluno.html";
 }
-function Cadastrar(){
+window.Cadastrar = function (){
     window.location.href = "/Inicial-tela/Cadastro/Cad.html";
 }
 function toggleCoor() {
@@ -73,12 +73,22 @@ function Formulario (event){
         // Salva no localStorage
         localStorage.setItem("iniciaisUsuario", iniciais);
         localStorage.setItem("nomeUsuario", nomeUsuario);
-        
-        // Redireciona
+        //Login com Supabase
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: senha,
+        });
+
+        if (error) {
+            alert("Erro: " + error.message);
+            return;
+        }
+
+        alert("Bem-vindo!");
         window.location.href = "/Professor/Index.html";
-        
+
     } else {
         alert("Email inválido. Use um email @professor.cps.sp.gov.br");
-        return false;
+        return;
     }
 }
